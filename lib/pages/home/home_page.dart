1218ca0cb63d8eb371ecf74681e54d9d2da4a8e4
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-06 20:19:05 +0530

    * Initial commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,150 @@
+import 'package:crypto_price_tracker/model/crypto_currency.dart';
+import 'package:crypto_price_tracker/provider/market_provider.dart';
+import 'package:crypto_price_tracker/provider/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'detail_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding:
+              const EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Current Crypto Price",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Consumer<ThemeProvider>(builder: (context, provider, child) {
+                    return IconButton(
+                      onPressed: () {
+                        provider.toggleTheme();
+                      },
+                      icon: provider.themeMode == ThemeMode.light
+                          ? const Icon(Icons.dark_mode)
+                          : const Icon(Icons.light_mode),
+                    );
+                  }),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: Consumer<MarketProvider>(
+                  builder: (context, provider, child) {
+                    if (provider.isLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (provider.markets.isNotEmpty) {
+                      return RefreshIndicator(
+                        onRefresh: () async {
+                          await provider.fetchData();
+                        },
+                        child: ListView.builder(
+                            physics: const BouncingScrollPhysics(
+                                parent: AlwaysScrollableScrollPhysics()),
+                            itemCount: provider.markets.length,
+                            itemBuilder: (context, index) {
+                              CryptoCurrency crypto = provider.markets[index];
+                              return ListTile(
+                                onTap: () {
+                                  showModalBottomSheet(
+                                      isScrollControlled: true,
+                                      context: context,
+                                      builder: (context) {
+                                        return SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .copyWith()
+                                                  .size
+                                                  .height *
+                                              0.85,
+                                          child: CryptoDetailPage(
+                                            id: crypto.id ?? "",
+                                          ),
+                                        );
+                                      });
+                                },
+                                contentPadding: const EdgeInsets.all(0),
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  backgroundImage:
+                                      NetworkImage(crypto.image ?? ""),
+                                ),
+                                title: Text(crypto.name ?? ""),
+                                subtitle:
+                                    Text((crypto.symbol ?? "").toUpperCase()),
+                                trailing: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      "₹ ${crypto.currentPrice?.toStringAsFixed(4) ?? ""}",
+                                      style: const TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.blue),
+                                    ),
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const Text(
+                                          "24h ",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.blueGrey),
+                                        ),
+                                        Builder(
+                                          builder: (context) {
+                                            double priceChange =
+                                                crypto.priceChange24 ?? 0;
+                                            double priceChangePercentage =
+                                                crypto.priceChangePercentage24 ??
+                                                    0;
+                                            return Text(
+                                              "${priceChangePercentage.toStringAsFixed(2)}% (${priceChange.toStringAsFixed(4)})",
+                                              style: TextStyle(
+                                                  color:
+                                                      priceChangePercentage > 0
+                                                          ? Colors.green
+                                                          : Colors.red),
+                                            );
+                                          },
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              );
+                            }),
+                      );
+                    } else {
+                      return const Text("data not found");
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
