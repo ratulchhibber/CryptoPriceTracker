0542a7802bb45b6f1d1da566f9b8d1d9774a4582
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-06 22:44:28 +0530

    * Adding TabBar to HomePage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:crypto_price_tracker/model/crypto_currency.dart';
+import 'package:crypto_price_tracker/pages/home/market_list.dart';
 import 'package:crypto_price_tracker/provider/market_provider.dart';
 import 'package:crypto_price_tracker/provider/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'detail_page.dart';
+import '../detail_page.dart';
+import 'favorite_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,7 +15,15 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,99 +56,30 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(
                 height: 20,
               ),
+              TabBar(controller: _tabController, tabs: [
+                Tab(
+                  child: Text(
+                    'All',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Favorites',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+              ]),
               Expanded(
-                child: Consumer<MarketProvider>(
-                  builder: (context, provider, child) {
-                    if (provider.isLoading) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else if (provider.markets.isNotEmpty) {
-                      return RefreshIndicator(
-                        onRefresh: () async {
-                          await provider.fetchData();
-                        },
-                        child: ListView.builder(
-                            physics: const BouncingScrollPhysics(
-                                parent: AlwaysScrollableScrollPhysics()),
-                            itemCount: provider.markets.length,
-                            itemBuilder: (context, index) {
-                              CryptoCurrency crypto = provider.markets[index];
-                              return ListTile(
-                                onTap: () {
-                                  showModalBottomSheet(
-                                      isScrollControlled: true,
-                                      context: context,
-                                      builder: (context) {
-                                        return SizedBox(
-                                          height: MediaQuery.of(context)
-                                                  .copyWith()
-                                                  .size
-                                                  .height *
-                                              0.85,
-                                          child: CryptoDetailPage(
-                                            id: crypto.id ?? "",
-                                          ),
-                                        );
-                                      });
-                                },
-                                contentPadding: const EdgeInsets.all(0),
-                                leading: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  backgroundImage:
-                                      NetworkImage(crypto.image ?? ""),
-                                ),
-                                title: Text(crypto.name ?? ""),
-                                subtitle:
-                                    Text((crypto.symbol ?? "").toUpperCase()),
-                                trailing: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: [
-                                    Text(
-                                      "₹ ${crypto.currentPrice?.toStringAsFixed(4) ?? ""}",
-                                      style: const TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.blue),
-                                    ),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        const Text(
-                                          "24h ",
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.blueGrey),
-                                        ),
-                                        Builder(
-                                          builder: (context) {
-                                            double priceChange =
-                                                crypto.priceChange24 ?? 0;
-                                            double priceChangePercentage =
-                                                crypto.priceChangePercentage24 ??
-                                                    0;
-                                            return Text(
-                                              "${priceChangePercentage.toStringAsFixed(2)}% (${priceChange.toStringAsFixed(4)})",
-                                              style: TextStyle(
-                                                  color:
-                                                      priceChangePercentage > 0
-                                                          ? Colors.green
-                                                          : Colors.red),
-                                            );
-                                          },
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              );
-                            }),
-                      );
-                    } else {
-                      return const Text("data not found");
-                    }
-                  },
+                child: TabBarView(
+                  physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics(),
+                  ),
+                  controller: _tabController,
+                  children: const [
+                    MarketList(),
+                    FavoriteList(),
+                  ],
                 ),
               ),
             ],
