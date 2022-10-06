0f5f76fa9c6a6418f8021a379f82e0d979b3ff20
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-07 04:09:18 +0530

    * Adding Favorite button- Since the row content(embedded under Expanded) may overlap with trailing element of ListView, we wrap the Text inside Flexible and setting fit to BoxFit.cover

--- lib/pages/home/market_list.dart
+++ lib/pages/home/market_list.dart
@@ -17,92 +17,129 @@ class _MarketListState extends State<MarketList> {
   Widget build(BuildContext context) {
     return Expanded(
       child: Consumer<MarketProvider>(
-        builder: (context, provider, child) {
-          if (provider.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (provider.markets.isNotEmpty) {
-            return RefreshIndicator(
-              onRefresh: () async {
+      builder: (context, provider, child) {
+        if (provider.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (provider.markets.isNotEmpty) {
+          return RefreshIndicator(
+            onRefresh: () async {
                 await provider.fetchData();
-              },
-              child: ListView.builder(
-                  physics: const BouncingScrollPhysics(
-                      parent: AlwaysScrollableScrollPhysics()),
-                  itemCount: provider.markets.length,
-                  itemBuilder: (context, index) {
-                    CryptoCurrency crypto = provider.markets[index];
-                    return ListTile(
-                      onTap: () {
-                        showModalBottomSheet(
-                            isScrollControlled: true,
-                            context: context,
-                            builder: (context) {
-                              return SizedBox(
-                                height: MediaQuery.of(context)
-                                        .copyWith()
-                                        .size
-                                        .height *
-                                    0.85,
-                                child: CryptoDetailPage(
-                                  id: crypto.id ?? "",
-                                ),
-                              );
-                            });
-                      },
-                      contentPadding: const EdgeInsets.all(0),
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        backgroundImage: NetworkImage(crypto.image ?? ""),
-                      ),
-                      title: Text(crypto.name ?? ""),
-                      subtitle: Text((crypto.symbol ?? "").toUpperCase()),
-                      trailing: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            "₹ ${crypto.currentPrice?.toStringAsFixed(4) ?? ""}",
-                            style: const TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue),
-                          ),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              const Text(
-                                "24h ",
-                                style: TextStyle(
-                                    fontSize: 12, color: Colors.blueGrey),
+            },
+            child: ListView.builder(
+                physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics()),
+                itemCount: provider.markets.length,
+                itemBuilder: (context, index) {
+                  CryptoCurrency crypto = provider.markets[index];
+                  return ListTile(
+                    onTap: () {
+                      showModalBottomSheet(
+                          isScrollControlled: true,
+                          context: context,
+                          builder: (context) {
+                            return SizedBox(
+                              height: MediaQuery.of(context)
+                                      .copyWith()
+                                      .size
+                                      .height *
+                                  0.85,
+                              child: CryptoDetailPage(
+                                id: crypto.id ?? "",
                               ),
-                              Builder(
-                                builder: (context) {
-                                  double priceChange =
-                                      crypto.priceChange24 ?? 0;
-                                  double priceChangePercentage =
-                                      crypto.priceChangePercentage24 ?? 0;
-                                  return Text(
-                                    "${priceChangePercentage.toStringAsFixed(2)}% (${priceChange.toStringAsFixed(4)})",
-                                    style: TextStyle(
-                                        color: priceChangePercentage > 0
-                                            ? Colors.green
-                                            : Colors.red),
-                                  );
+                            );
+                          });
+                    },
+                    contentPadding: const EdgeInsets.all(0),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      backgroundImage: NetworkImage(crypto.image ?? ""),
+                    ),
+                    title: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Flexible(
+                          child: FittedBox(
+                            fit: BoxFit.cover,
+                            child: Text(crypto.name ?? ""),
+                          ),
+                        ),
+                        Consumer<FavoriteProvider>(
+                          builder: (context, favoriteProvider, child) {
+                            return IconButton(
+                              onPressed: () {
+                                favoriteProvider.toggleState(crypto.id ?? "");
+                              },
+                              icon: FutureBuilder<Favorite>(
+                                future: favoriteProvider
+                                    .fetchState(crypto.id ?? ""),
+                                builder: (context, snapshot) {
+                                  switch (snapshot.connectionState) {
+                                    case ConnectionState.waiting:
+                                      return const Text('');
+                                    default:
+                                      if (snapshot.hasError) {
+                                        return const Text('');
+                                      } else {
+                                        return Icon(
+                                          snapshot.requireData.icon,
+                                          color: snapshot.requireData.iconColor,
+                                        );
+                                      }
+                                  }
                                 },
                               ),
-                            ],
-                          )
-                        ],
-                      ),
-                    );
-                  }),
-            );
-          } else {
-            return const Text("data not found");
-          }
-        },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                    subtitle: Text((crypto.symbol ?? "").toUpperCase()),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "₹ ${crypto.currentPrice?.toStringAsFixed(4) ?? ""}",
+                          style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              "24h ",
+                              style: TextStyle(
+                                  fontSize: 12, color: Colors.blueGrey),
+                            ),
+                            Builder(
+                              builder: (context) {
+                                double priceChange = crypto.priceChange24 ?? 0;
+                                double priceChangePercentage =
+                                    crypto.priceChangePercentage24 ?? 0;
+                                return Text(
+                                  "${priceChangePercentage.toStringAsFixed(2)}% (${priceChange.toStringAsFixed(4)})",
+                                  style: TextStyle(
+                                      color: priceChangePercentage > 0
+                                          ? Colors.green
+                                          : Colors.red),
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          );
+        } else {
+          return const Text("data not found");
+        }
+      },
       ),
     );
   }
