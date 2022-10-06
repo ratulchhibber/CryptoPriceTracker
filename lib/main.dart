1ff667baf461c68dad3e60c7413fdac8d6f85eda
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-06 22:45:59 +0530

    * MultiProvider on main.dart and using Consumer herein for ThemeProvider to switch between theme variants

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,68 @@
+import 'package:crypto_price_tracker/pages/home/home_page.dart';
+import 'package:crypto_price_tracker/provider/market_provider.dart';
+import 'package:crypto_price_tracker/provider/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:crypto_price_tracker/theme/theme.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'constant/local_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    FutureBuilder<ThemeMode>(
+      future: LocalStorage.fetchTheme(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Directionality(
+              textDirection: TextDirection.ltr,
+              child: Text('Loading....'),
+            );
+          default:
+            if (snapshot.hasError) {
+              return Directionality(
+                textDirection: TextDirection.ltr,
+                child: Text('Error: ${snapshot.error}'),
+              );
+            } else {
+              return MyApp(themeMode: snapshot.requireData);
+            }
+        }
+      },
+    ),
+  );
+
+  // runApp(MyApp(themeMode: LocalStorage.fetchTheme()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  ThemeMode themeMode;
+  MyApp({Key? key, required this.themeMode}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MarketProvider>(
+          create: (context) => MarketProvider(),
+        ),
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (context) => ThemeProvider(themeMode),
+        ),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, provider, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            themeMode: provider.themeMode,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            home: const HomePage(),
+          );
+        },
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
