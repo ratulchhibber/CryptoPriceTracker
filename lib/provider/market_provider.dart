5781adeeb9901ae00eda8f8b1b721769dbefe18c
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-07 17:54:38 +0530

    * Updations to FavoriteProvides, it would now incorporate the logic for favoriting a crypto currency

--- lib/provider/market_provider.dart
+++ lib/provider/market_provider.dart
@@ -1,22 +1,31 @@
+import 'package:crypto_price_tracker/constant/storage/favorite_storage.dart';
+import 'package:crypto_price_tracker/model/favorite.dart';
 import 'package:flutter/material.dart';
 
 import '../api/api.dart';
 import '../model/crypto_currency.dart';
+import '../pages/home/market_list.dart';
 
 class MarketProvider with ChangeNotifier {
   bool isLoading = true;
   List<CryptoCurrency> markets = [];
 
-  MarketProvider() {
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
+  Future<void> fetchData(ListType type) async {
     List<dynamic> markets = await API.getMarkets();
     this.markets.clear();
     for (var market in markets) {
       CryptoCurrency newCrypto = CryptoCurrency.fromJSON(market);
+      newCrypto.isFavorite =
+          await FavoriteStorage().fetchState(newCrypto.id ?? "");
       this.markets.add(newCrypto);
+    }
+
+    if (type == ListType.favorites) {
+      List<CryptoCurrency> temp = this
+          .markets
+          .where((element) => element.isFavorite == Favorite.enabled)
+          .toList();
+      this.markets = temp;
     }
     isLoading = false;
     notifyListeners();
